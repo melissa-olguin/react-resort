bb2c9b046ce67385dffc51524fcab38fa1ebfbe0
Melissa Olguin <57164061+melissa-olguin@users.noreply.github.com>
2020-02-13 00:19:42 -0600

    finishing touch

--- src/App.js
+++ src/App.js
@@ -7,13 +7,16 @@ import SingleRoom from "./pages/SingleRoom";
 import Error from "./pages/Error";
 
 import Navbar from "./components/Navbar";
-
+import { Helmet } from "react-helmet";
 import { Switch, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Beach Resort</title>
+      </Helmet>
       <Navbar />
       <Switch>
         <Route exact path="/" component={Home} />
